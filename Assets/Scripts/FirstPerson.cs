9ef09d0a3318bc32a267f113f2616301a2c29215
Kevin Ray <kr1995@nyu.edu>
2018-10-01 12:27:03 -0400

    Initial Commit

---
+++ Assets/Scripts/FirstPerson.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.UIElements;
+using UnityEngine;
+
+public class FirstPerson : MonoBehaviour
+{
+	public float moveSpeed = 10.0f;
+
+	private float lookSpeed = 500.0f;
+
+	private Vector3 inputVector;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float mouseX = Input.GetAxis(("Mouse X")) *lookSpeed*Time.deltaTime;
+		float mouseY = Input.GetAxis(("Mouse Y"))*lookSpeed*Time.deltaTime;
+		transform.Rotate(0,mouseX,0);
+		Camera.main.transform.Rotate(-mouseY,0,0);
+		Camera.main.transform.localEulerAngles -= new Vector3(0,0,Camera.main.transform.localEulerAngles.z);
+
+		float vertical = Input.GetAxis(("Vertical"));
+		float horizontal = Input.GetAxis(("Horizontal"));
+
+		inputVector = transform.forward * vertical * moveSpeed;
+		inputVector += transform.right * horizontal * moveSpeed;
+
+	}
+
+	void FixedUpdate()
+	{
+		GetComponent<Rigidbody>().velocity = inputVector;
+	}
+}
---
+++ Assets/Scripts/ThrowScript.cs
@@ -0,0 +1,216 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThrowScript : MonoBehaviour
+{
+	public float PWR = 0.0f; //P
+	public float AIM = 0.0f; //A
+	public float SPD = 0.0f; //K
+	public float BAL = 0.0f; //B
+
+
+	private bool throwP = false;
+	private bool throwA = false;
+	private bool throwK = false;
+	private bool throwB = false;
+	private bool THROW = false;
+	public bool levelOne = true;
+	public bool levelTwo = false;
+	public bool levelThree = false;
+	private bool hunCheckP = false;
+	private bool hunCheckA = false;
+	private bool hunCheckK = false;
+	private bool hunCheckB = false;
+
+	public Image PWRBar;
+	public Image AIMBar;
+	public Image SPDBar;
+	public Image BALBar;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		PWRBar.fillAmount = PWR / 100;
+		AIMBar.fillAmount = AIM / 100;
+		SPDBar.fillAmount = SPD / 100;
+		BALBar.fillAmount = BAL / 100;
+		//LEVEL ONE POWER AND AIM
+		if (levelOne)
+		{
+			if (!throwP)
+			{
+				if (Input.GetKey(KeyCode.P))
+				{
+					if (PWR >= 99)
+						hunCheckP = true;
+					if (PWR <= 1)
+						hunCheckP = false;
+					if (!hunCheckP)
+						PWR++;
+					if (hunCheckP)
+						PWR--;
+				}
+
+				if (Input.GetKeyUp(KeyCode.P))
+					throwP = true;
+			}
+
+			if (!throwA)
+			{
+				if (Input.GetKey(KeyCode.A))
+				{
+					if (AIM >= 99)
+						hunCheckA = true;
+					if (AIM <= 1)
+						hunCheckA = false;
+					if (!hunCheckA)
+						AIM++;
+					if (hunCheckA)
+						AIM--;
+				}
+
+				if (Input.GetKeyUp(KeyCode.A))
+					throwA = true;
+			}
+		}
+		//LEVEL TWO POWER AND AIM AND SPEED
+		if (levelTwo)
+		{
+			if (!throwP)
+			{
+				if (Input.GetKey(KeyCode.P))
+				{
+					if (PWR >= 99)
+						hunCheckP = true;
+					if (PWR <= 1)
+						hunCheckP = false;
+					if (!hunCheckP)
+						PWR++;
+					if (hunCheckP)
+						PWR--;
+				}
+
+				if (Input.GetKeyUp(KeyCode.P))
+					throwP = true;
+			}
+
+			if (!throwA)
+			{
+				if (Input.GetKey(KeyCode.A))
+				{
+					if (AIM >= 99)
+						hunCheckA = true;
+					if (AIM <= 1)
+						hunCheckA = false;
+					if (!hunCheckA)
+						AIM++;
+					if (hunCheckA)
+						AIM--;
+				}
+
+				if (Input.GetKeyUp(KeyCode.A))
+					throwA = true;
+			}
+
+			if (!throwK)
+			{
+				if (Input.GetKey(KeyCode.K))
+				{
+					if (SPD >= 99)
+						hunCheckK = true;
+					if (SPD <= 1)
+						hunCheckK = false;
+					if (!hunCheckK)
+						SPD++;
+					if (hunCheckK)
+						SPD--;
+				}
+
+				if (Input.GetKeyUp(KeyCode.K))
+					throwK = true;
+			}
+		}
+		//LEVEL THREE POWER AND AIM AND SPEED AND BALANCE
+		if (levelThree)
+		{
+			if (!throwP)
+			{
+				if (Input.GetKey(KeyCode.P))
+				{
+					if (PWR >= 99)
+						hunCheckP = true;
+					if (PWR <= 1)
+						hunCheckP = false;
+					if (!hunCheckP)
+						PWR++;
+					if (hunCheckP)
+						PWR--;
+				}
+
+				if (Input.GetKeyUp(KeyCode.P))
+					throwP = true;
+			}
+
+			if (!throwA)
+			{
+				if (Input.GetKey(KeyCode.A))
+				{
+					if (AIM >= 99)
+						hunCheckA = true;
+					if (AIM <= 1)
+						hunCheckA = false;
+					if (!hunCheckA)
+						AIM++;
+					if (hunCheckA)
+						AIM--;
+				}
+
+				if (Input.GetKeyUp(KeyCode.A))
+					throwA = true;
+			}
+
+			if (!throwK)
+			{
+				if (Input.GetKey(KeyCode.K))
+				{
+					if (SPD >= 99)
+						hunCheckK = true;
+					if (SPD <= 1)
+						hunCheckK = false;
+					if (!hunCheckK)
+						SPD++;
+					if (hunCheckK)
+						SPD--;
+				}
+
+				if (Input.GetKeyUp(KeyCode.K))
+					throwK = true;
+			}
+			if (!throwB)
+			{
+				if (Input.GetKey(KeyCode.B))
+				{
+					if (BAL >= 99)
+						hunCheckB = true;
+					if (BAL <= 1)
+						hunCheckB = false;
+					if (!hunCheckB)
+						BAL++;
+					if (hunCheckB)
+						BAL--;
+				}
+
+				if (Input.GetKeyUp(KeyCode.B))
+					throwB = true;
+			}
+		}
+	}
+}
